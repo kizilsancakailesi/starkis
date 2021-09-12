fc8e5daddc879bfab7e5a4b3106c1c55d9d97a1e
KIZILSANCAK AİLESİ <90479077+kizilsancakailesi@users.noreply.github.com>
2021-09-12 15:54:52 +0300

    Update luna.py

--- luna.py
+++ luna.py
@@ -53,8 +53,8 @@ async def type_and_send(message):
 @luna.on_message(filters.command("repo") & ~filters.edited)
 async def repo(_, message):
     await message.reply_text(
-        "[GitHub](https://github.com/thehamkercat/LunaChatBot)"
-        + " | [Group](t.me/PatheticProgrammers)",
+        "[ADSIZ KAPTAN](https://t.me/kizilsancakbilgi)"
+        + " | [Group](t.me/sancakailesi)",
         disable_web_page_preview=True,
     )
 
